45d525bf1b737f438d3d9332daa18f5520eed8ec
is221827 <is221827@fhstp.ac.at>
2023-03-10 19:50:30 +0100

    webstuff corrections (img needs alt)

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -12,6 +12,7 @@ const CartItem = props => {
             <figure className="image is-64x64">
               <img
                 src="https://bulma.io/images/placeholders/128x128.png"
+                alt={product.article_name}
               />
             </figure>
           </div>
--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -10,7 +10,7 @@ const ProductItem = props => {
             <figure className="image is-64x64">
               <img
                 src="https://bulma.io/images/placeholders/128x128.png"
-                //alt={product.shortDesc}
+                alt={product.article_name}
               />
             </figure>
           </div>
