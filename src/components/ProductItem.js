70d84831d234a3b639dd99fe6cb3acb15cbb569e
is221827 <is221827@fhstp.ac.at>
2023-03-14 23:38:22 +0100

    third typo

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ImageComponent from "./ImageComponent";
+import ImageComponent from "./imageComponent";
 //<ImageComponent src={'https://sapicture.blob.core.windows.net/sa-c-picture/'+product.article_number+'.jpg'}/>
 /*<img
 src={'https://sapicture.blob.core.windows.net/sa-c-picture/'+product.article_number+'.jpg'}
