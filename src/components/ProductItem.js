1b51e240bd1705cfb863e40f7c80274d25889572
is221827 <is221827@fhstp.ac.at>
2023-03-14 23:28:48 +0100

    imageComponent imported

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ImageComponent from "./component/ImageComponent";
 //<ImageComponent src={'https://sapicture.blob.core.windows.net/sa-c-picture/'+product.article_number+'.jpg'}/>
 /*<img
 src={'https://sapicture.blob.core.windows.net/sa-c-picture/'+product.article_number+'.jpg'}
