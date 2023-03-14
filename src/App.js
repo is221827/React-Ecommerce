7a351c70a4cb262c9f8f7e62961388a6a4867629
is221827 <is221827@fhstp.ac.at>
2023-03-14 20:31:20 +0100

    cors setup changed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
 import { Switch, Route, Link, BrowserRouter as Router } from "react-router-dom";
 import axios from 'axios';
-//import jwt_decode from 'jwt-decode';
-
-//import AddProduct from './components/AddProduct';
-//import Cart from './components/Cart';
 import ProductList from './components/ProductList';
 
 import Context from "./Context";
@@ -19,20 +15,13 @@ export default class App extends Component {
   }
 
   async componentDidMount() {
-    const products = await axios.get('https://webshop-fe.azurewebsites.net/api/products');
+    const products = await axios.get('http://webshop-fe.azurewebsites.net/api/products/');
     this.setState({ products: products.data });
   }
 
-  /*addProduct = (product, callback) => {
-    let products = this.state.products.slice();
-    products.push(product);
-    //??
-    this.setState({ products }, () => callback && callback());
-  };*/
-
   buyProduct = articleId => {
     if (this.state.products[articleId].amount > 0) {
-      const response = axios.get('https://webshop-fe.azurewebsites.net/api/order/'+articleId);
+      const response = axios.get('http://webshop-fe.azurewebsites.net/api/order/'+articleId);
       if (response.status === 200) {
         let products = this.state.products.slice();
         products[articleId].amount -= 1;
@@ -42,49 +31,7 @@ export default class App extends Component {
         return "nono";
       }
     }
-    /*let cart = this.state.cart;
-    if (cart[cartItem.id]) {
-      cart[cartItem.id].amount += cartItem.amount;
-    } else {
-      cart[cartItem.id] = cartItem;
-    }
-    //there might be a fuckup here. product.stock????
-    if (cart[cartItem.id].amount > cart[cartItem.id].product.stock) {
-      cart[cartItem.id].amount = cart[cartItem.id].product.stock;
-    }
-    localStorage.setItem("cart", JSON.stringify(cart));
-    this.setState({ cart });*/
   };
-
-  /*removeFromCart = cartItemId => {
-    let cart = this.state.cart;
-    delete cart[cartItemId];
-    localStorage.setItem("cart", JSON.stringify(cart));
-    this.setState({ cart });
-  };
-
-  clearCart = () => {
-    let cart = {};
-    localStorage.removeItem("cart");
-    this.setState({ cart });
-  };
-
-  checkout = () => {
-    const cart = this.state.cart;
-
-    const products = this.state.products.map(p => {
-      if (cart[p.name]) {
-        p.stock = p.stock - cart[p.name].amount;
-
-        axios.get('https://webshop-fe.azurewebsites.net/api/order/%7Barticleid%7D')
-        // here is sms stuff missing
-      }
-      return p;
-    });
-
-    this.setState({ products });
-    this.clearCart();
-  };*/
 
   render() {
     return (
